4fd24074e23677556e229c199247f10a4afdcef3
Benjamin Slade <slade@lambda-y.net>
2025-03-24 20:13:56 -0500

    various changes
    [unhelpful changelog]

--- sysinfo-environs.el
+++ sysinfo-environs.el
@@ -57,6 +57,16 @@
   :group 'sysinfo-environs
   :type 'boolean)
 
+(defcustom sysinfo-environs-show-os-release-logo-image t
+  "Show logo image if possible when `os-release' is pared."
+  :group 'sysinfo-environs
+  :type 'boolean)
+
+(defcustom sysinfo-environs-neofetch-no-single-window nil
+  "If set, don't single-window neofetch-info."
+  :group 'sysinfo-environs
+  :type 'boolean)
+
 ;;;; Helper functions
 ;;;###autoload
 (defun sysinfo-environs-newlines-string-into-line-list (longstring)
@@ -201,6 +211,8 @@ Assumes operating system ID to be `OS-ID-NAME'.
           (cons "*os-release info*"
                 (cons
                  (cons "`os-release' location" os-release-file)
+                 ;; TODO:
+                 ;; maybe test for symlink and show pointer??
                  (when os-release-raw
                    (sysinfo-environs-list-of-string-equals-string-into-alist
                     os-release-raw t)))))
@@ -210,11 +222,7 @@ Assumes operating system ID to be `OS-ID-NAME'.
                      (if sysinfo-environs-try-to-find-unspecified-logo
                          os-id-name
                        nil))))
-    ;; (list logo-name os-id-name)
-    ;; (list os-release-file os-release-raw os-release)
-    ;; (list os-release-file os-release-raw os-release logo-name)
-    ;; os-release
-
+    ;; find logo image
     (let ((img-path (if logo-name 
                         (sysinfo-environs--find-logo-path
                          logo-name os-id-name)
@@ -226,8 +234,10 @@ Assumes operating system ID to be `OS-ID-NAME'.
                                  (if img-path
                                      img-path
                                    "[not found]"))
-                           (nreverse os-release)))))
-      os-release)))
+                           (nreverse os-release)
+                           ;; (nreverse (cl-remove nil os-release :test 'equal))
+                           ))))
+      (cl-remove nil os-release :test 'equal))))
 
 ;; (cdr (assoc "LOGO" (cdr (sysinfo-environs-parse-os-release))))
 ;; (setq sysinfo-raws (sysinfo-environs-parse-os-release))
@@ -258,7 +268,8 @@ Assumes operating system ID to be `OS-ID-NAME'.
                                                    (shell-command-to-string
                                                     (concat "uname " value))))
                    uname-info)))
-    (cons "*uname -?*" uname-info)))
+    (cons "*uname -?*"
+          (cl-remove nil uname-info :test 'equal))))
 
 ;;;;; sysinfo from Emacs itself
 
@@ -378,12 +389,122 @@ Assumes operating system ID to be `OS-ID-NAME'.
 ;;        ((eq sco-state t)
 ;;         )))))
 
+;;;;; $ENV
+(defun sysinfo-environs-get-all-env ()
+  "Unused function for $ENV things."
+  (cons "*environment variables*"
+        (cl-remove '("") 
+                   (sysinfo-environs-list-of-string-equals-string-into-alist
+                    (sysinfo-environs-newlines-string-into-line-list
+                     (shell-command-to-string "printenv")))
+                   :test 'equal)))
+
+(defun sysinfo-environs-gui-env ()
+  "Return likely GUI environment (i.e., WM/DE)."
+  (let ((xdg-current-desktop-top
+         (if (sysinfo-environs-look-up-field "env-vars-info" "XDG_CURRENT_DESKTOP")
+           (car 
+            (split-string  ;; in-case it's a :'ed PATH-type return
+              (sysinfo-environs-look-up-field "env-vars-info" "XDG_CURRENT_DESKTOP")
+              ":"))
+           ""))
+        (xdg-session-desktop
+         (sysinfo-environs-look-up-field "env-vars-info" "XDG_SESSION_DESKTOP"))
+        (session-desktop
+         (sysinfo-environs-look-up-field "env-vars-info" "DESKTOP_SESSION")))
+    (cond ((not (or (string-empty-p xdg-current-desktop-top)
+                    (null xdg-current-desktop-top)))
+           xdg-current-desktop-top)
+          ((not (or (string-empty-p xdg-session-desktop)
+                    (null xdg-session-desktop)))
+           xdg-session-desktop)
+          ((not (or (string-empty-p session-desktop)
+                    (null session-desktop)))
+           session-desktop)
+          ;; check for wmctrl...
+          (t nil))))
+
+;; (setq my/wmde (sysinfo-environs-gui-env))
+;; (setq sysinfo-env-vars (sysinfo-environs-get-all-env))
+
+;;;;; /sys/devices/virtual/dmi/id
+(defun sysinfo-environs-read-sys-devices-virtual-dmi-ids ()
+  "Get accessible hardware info encoded in `/sys/devices/virtual/dmi/id'."
+(let* ((virtual-device-ids-dir "/sys/devices/virtual/dmi/id")
+       (virtual-device-ids
+        (split-string (shell-command-to-string
+                       (concat "ls " virtual-device-ids-dir))))
+       (hardsysinfo nil))
+  (dolist (item virtual-device-ids)
+    (let* ((item-file (concat virtual-device-ids-dir "/" item))
+           (item-content
+            (if (and (f-file-p item-file) (file-readable-p item-file))
+                (replace-regexp-in-string "\n" ""
+                                          (f-read-text item-file))
+              nil)))
+      (when item-content
+        (setq hardsysinfo
+              (cons
+               (cons item
+                     item-content)
+               hardsysinfo)))))
+  (cons "*virtual dev info*"
+   hardsysinfo)))
+
+;; (setq hardtesty (sysinfo-environs-read-sys-devices-virtual-dmi-ids))
+
+;;;;; network
+
+;; (format "%X:%X:%X" 65152 0 4762)
+
+;; TRY....
+;; (let ((out ""))
+;;   (dolist (val '(65152 0 0 0 4762 56831 65182 36611 0))
+;;     (setq out (concat out
+;;                (format "%X:" val))))
+;;   out)
+
+;; (let ((out nil))
+;;   (dolist (val '(65152 0 0 0 4762 56831 65182 36611 0))
+;;     (setq out (cons
+;;                (format "%X" val)
+;;                out)))
+;;   (setq out (nreverse out))
+;;   (dolist (seg out)
+;;     ))
+;; END TRY....
+
+;; two or more 0000 blocks change to ::, but only one group (with most in row)
+
+
+;; (network-interface-list)(("wlan0" . [65152 0 0 0 4762 56831 65182 36611 0]) ("lo" . [0 0 0 0 0 0 0 1 0]) ("wlan0" . [192 168 0 217 0]) ("lo" . [127 0 0 1 0]))
+
+;; (let ((interfaces nil))
+;;   (dolist (interface (network-interface-list))
+;;     (let* ((name (car interface))
+;;            (addr (cdr interface))
+;;            (lo-p (if (string= name "lo") t nil))
+;;            (ip4-p (if (< (length addr) 6) t nil))
+;;            (ip (if ip4-p
+;;                  (format "%s.%s.%s.%s"
+;;                          (nth 0 addr)
+;;                          (nth 1 addr)
+;;                          (nth 2 addr)
+;;                          (nth 3 addr))
+;;                  (format "%X:%X:%X")))))
+;;     (setq interfaces
+;;           (cons
+;;            (cons )
+;;            interfaces))))
+
 ;;;;; list of all sources
 (defvar sysinfo-environs-dataset-alist
   '(("emacs-info" . (sysinfo-environs-emacs-known-sysinfo))
     ("emacs-self-info" . (sysinfo-environs-emacs-self-info))
     ("uname-info" . (sysinfo-environs-parse-uname-info))
-    ("os-release-info" . (sysinfo-environs-parse-os-release)))
+    ("os-release-info" . (sysinfo-environs-parse-os-release))
+    ("env-vars-info" . (sysinfo-environs-get-all-env))
+    ("sys-devices-virtual-dmi-info" . (sysinfo-environs-read-sys-devices-virtual-dmi-ids)))
   "An alist of all of the datasources and some names for them.")
 
 (defun sysinfo-environs-dataset-bare-list ()
@@ -392,12 +513,15 @@ For use in helper functions and elsewhere."
   (let ((barelist nil))
     (dolist (dataset sysinfo-environs-dataset-alist)
       (setq barelist
-            (append (list (eval (list (cadr dataset))))
+            (cons (eval (list (cadr dataset)))
                   barelist)))
-    (nreverse barelist)))
+    (nreverse 
+     (cl-remove nil barelist :test 'equal))))
 
 
 ;; (sysinfo-environs-emacs-known-sysinfo)
+;; (sysinfo-environs-dataset-bare-list)
+;; (setq testy (sysinfo-environs-dataset-bare-list))
 ;; (sysinfo-environs-dataset-bare-list-quoted)
 
 ;;;; Main functions  for creating temp buffers with pretty org tables
@@ -464,7 +588,7 @@ and then listing all possible `FIELDNAME's for chosen data source."
 ;;; Main function
 
 ;;;###autoload
-(defun sysinfo-environs-sysinfo (datasets &optional titlename)
+(defun sysinfo-environs-sysinfo (datasets &optional titlename show-logo)
   "Create temp buffers with Org tables containing system information.
 
 Should be called with a list of one or more `DATASETS'
@@ -487,12 +611,6 @@ Should be called with a list of one or more `DATASETS'
           (insert "|----|----|\n")
           (with-current-buffer sub-temp-buff
             (dolist (item sysinfo)
-              ;; (when (and (string= dtitle "*os-release info*")
-              ;;            (string= (car item) "LOGO"))
-              ;;   (setq logo-name (cdr item)))
-              ;; (when (and (string= dtitle "*os-release info*")
-              ;;            (string= (car item) "ID"))
-              ;;   (setq os-id-name (cdr item)))
               (let* ((label (car item))
                      (value* (cdr item))
                      (value (cond ((stringp value*)
@@ -519,45 +637,120 @@ Should be called with a list of one or more `DATASETS'
       (when (and (boundp 'visual-fill-column-mode)
                  visual-fill-column-mode)
         (visual-fill-column-mode -1)))
-    ;; (when logo-name
-    ;;   (let ((xdg-dirs (list
-    ;;                    (concat "/usr/share/" os-id-name)
-    ;;                    "/usr/share/pixmaps"
-    ;;                    "/usr/local/share/icons"
-    ;;                    "/usr/share/icons"
-    ;;                    "/run/current-system/profile/share/icons"))
-    ;;         (logo-search '()))
-    ;;     (dolist (dir xdg-dirs)
-    ;;       (when (file-directory-p dir)
-    ;;         (setq logo-search
-    ;;               (if (executable-find "find")
-    ;;                   (split-string
-    ;;                    (shell-command-to-string (concat "find " dir " -iname \"*" logo-name "*\" -exec realpath {} \\;")) "\n")
-    ;;                 (directory-files-recursively dir (concat logo-name ".*"))))))
-    ;;     (dolist (betterimage
-    ;;              '(
-    ;;                "svg$"
-    ;;                "1024x1024" "512x512" "256x256" "128x128"
-    ;;                )
-    ;;              )
-    ;;       (setq logo-search-temp logo-search)
-    ;;       (while (and (= (length best-image) 0) logo-search-temp)
-    ;;         (let ((imagehit (car logo-search-temp)))
-    ;;           (when (string-match betterimage imagehit)
-    ;;             (setq best-image imagehit))
-    ;;           (setq logo-search-temp (cdr logo-search-temp)))))))
       (with-current-buffer temp-buff-name
         (read-only-mode -1)
         (goto-char (point-max))
-        (let ((img-path (sysinfo-environs-look-up-field "os-release-info" "LOGO-IMAGE-PATH")))
-          (when (and img-path
-                     (not (string= img-path "[not found]")))
-            (insert (concat "[[" img-path "]]"))))
-        (org-link-preview)
+        (when (and sysinfo-environs-show-os-release-logo-image
+               show-logo)
+          (let ((img-path (sysinfo-environs-look-up-field "os-release-info" "LOGO-IMAGE-PATH")))
+            (when (and img-path
+                       (not (string= img-path "[not found]")))
+              (insert (concat "[[" img-path "]]"))))
+          (org-link-preview))
         (read-only-mode 1)
         (goto-char (point-min)))))
 
-;;;; Interactive functions
+;;;; uptime
+(defun sysinfo-environ-sys-uptime ()
+  "Get system uptime."
+  (interactive)
+  (let ((cat-proc-uptime
+         (string-to-number
+          (car 
+           (split-string 
+            (shell-command-to-string "cat /proc/uptime"))))))
+    (format-seconds "%Y, %D, %H, %M, %z%S" cat-proc-uptime)))
+
+;; (setq catup (sysinfo-environ-sys-uptime))
+
+;;;; (n)E(o)fetch
+
+;;;###autoload
+(defun sysinfo-environs-neofetch-like (&optional no-logo titlename)
+  "Interactive function sort of like (neo|fast)fetch."
+  (interactive)
+  (defalias 'lookup 'sysinfo-environs-look-up-field)
+  (let* ((machine (lookup "sys-devices-virtual-dmi-info" "product_version"))
+         (machine (if (string-empty-p machine)
+                      (lookup "sys-devices-virtual-dmi-info" "product_family")
+                    machine)))
+    (let ((temp-buff-name (or titlename "*(n)E(o)fetch*"))
+          (neofetch `(("OS" .
+                       ,(concat
+                         (lookup "os-release-info" "PRETTY_NAME")
+                         " "
+                         system-configuration))
+                      ("Host" .
+                       ,(concat
+                         (lookup "sys-devices-virtual-dmi-info" "product_name")
+                         " ("
+                         machine ;; product_version or product_family
+                         ")"))
+                      ("Kernel" .
+                       ,(concat
+                        (lookup "uname-info" "KERNEL_NAME")
+                        " "
+                        (lookup "uname-info" "KERNEL_RELEASE")))
+                      ("System Uptime" .
+                       ,(sysinfo-environ-sys-uptime))
+                      ("Emacs Uptime" .
+                       ,(emacs-uptime))
+                      ("WM/DE" .
+                       ,(concat
+                         (sysinfo-environs-gui-env)
+                         " ("
+                         (lookup "env-vars-info" "XDG_SESSION_TYPE")
+                         ")"))
+                      )))
+      (get-buffer-create temp-buff-name)
+      (with-current-buffer temp-buff-name
+        (read-only-mode -1)
+        (erase-buffer)
+        (insert "|----|\n")
+        (insert "|" " ~" user-login-name"~​" "@​=" (system-name)"=" "|\n")
+        (insert "|----|\n")
+        (dolist (item neofetch)
+          (let ((field (car item))
+                (val (cdr item)))
+            ;; ....
+            (insert "| =" field "=: " "~" val "~" "|\n")
+            )
+          )
+        (insert "|----|\n")
+        (org-mode)
+        (goto-char (point-min))
+        (unless (equal no-logo t)
+          ;; (org-table-insert-column)
+          ;; (next-line)
+          ;; (next-line)
+          (let
+              ((img-path
+                (sysinfo-environs-look-up-field "os-release-info" "LOGO-IMAGE-PATH")))
+            (when (and img-path
+                       (not (string= img-path "[not found]")))
+              (insert "\n")
+              (insert (concat "[[" img-path "]]"))
+              (insert "\n\n")))
+          (org-link-preview))
+        (org-table-align)
+        (org-table-align) ;; need to do it twice for some reason
+        (goto-char (point-min))
+        ;; (insert "\n\n")
+        ;; (goto-char (point-max))
+        ;; (insert "\n")
+        (switch-to-buffer-other-window temp-buff-name)
+        (when (and (boundp 'visual-fill-column-mode)
+                   visual-fill-column-mode)
+          (visual-fill-column-mode -1))
+        (read-only-mode 1)
+        (goto-char (point-min))
+        (unless sysinfo-environs-neofetch-no-single-window
+          (delete-other-windows))))))
+
+;; (sysinfo-environs-neofetch-like)
+
+
+;;;; Interactive convenience functions
 
 ;;;###autoload
 (defun sysinfo-environs-os-release-info-display ()
@@ -566,7 +759,8 @@ Should be called with a list of one or more `DATASETS'
   (sysinfo-environs-sysinfo
    (list
     (sysinfo-environs-parse-os-release))
-   "*OS Release Info*"))
+   "*OS Release Info*"
+   t))
 
 ;;;###autoload
 (defun sysinfo-environs-os-uname-info-display ()
@@ -585,7 +779,8 @@ Should be called with a list of one or more `DATASETS'
    (list
     (sysinfo-environs-parse-uname-info)
     (sysinfo-environs-parse-os-release))
-   "*System Info*"))
+   "*System Info*"
+   t))
 
 ;;;###autoload
 (defun sysinfo-environs-emacs-self-info-display ()
@@ -596,43 +791,93 @@ Should be called with a list of one or more `DATASETS'
     (sysinfo-environs-emacs-self-info))
    "*Emacs Self Info*"))
 
+;;;###autoload
+(defun sysinfo-environs-env-display ()
+  "Interactive function to display environment info."
+  (interactive)
+  (sysinfo-environs-sysinfo
+   (list
+    (sysinfo-environs-get-all-env))
+   "*ENV info*"))
 
 ;;;###autoload
-(defun sysinfo-environs-full-sys-info-display ()
+(defun sysinfo-environs-virtual-dmi-info-display ()
+  "Interactive function to display virtual dmi/dev info."
+  (interactive)
+  (sysinfo-environs-sysinfo
+   (list
+    (sysinfo-environs-read-sys-devices-virtual-dmi-ids))
+   "*virtual dmi info*"))
+
+
+
+;;;###autoload
+(defun sysinfo-environs-fullish-sys-info-display ()
+  "Interactive function to display all accessible system info."
+  (interactive)
+  (sysinfo-environs-sysinfo
+   (list
+    (sysinfo-environs-emacs-known-sysinfo)
+    (sysinfo-environs-emacs-self-info)
+    (sysinfo-environs-parse-uname-info)
+    (sysinfo-environs-parse-os-release)
+    ;; (sysinfo-environs-read-sys-devices-virtual-dmi-ids)
+    )
+    "*System Info*"
+    t))
+
+
+;;;###autoload
+(defun sysinfo-environs-fullest-sys-info-display ()
   "Interactive function to display all accessible system info."
   (interactive)
   (sysinfo-environs-sysinfo
    (sysinfo-environs-dataset-bare-list)
-   "*System Info*"))
+   "*System Info*"
+   t))
 
 
 ;; (sysinfo-environs-os-release-info)
 
 ;;; printenv things, probably not good
-(defun sysinfo-environs-split-paths-with-newlines (input)
-  "Unused function for $ENV things from `INPUT'."
-  (let ((new-alist nil))
-    (dolist (item input)
-      (setq new-alist
-            (cons
-             (cons
-              (car item)
-              (if (not (null (cdr item)))
-                       (replace-regexp-in-string ":" ":\n"
-                                                 (cdr item))
-                (cdr item)))
-             new-alist)))
-    new-alist))
+;; (defun sysinfo-environs-split-paths-with-newlines (input)
+;;   "Unused function for $ENV things from `INPUT'."
+;;   (let ((new-alist nil))
+;;     (dolist (item input)
+;;       (setq new-alist
+;;             (cons
+;;              (cons
+;;               (car item)
+;;               (if (not (null (cdr item)))
+;;                        (replace-regexp-in-string ":" ":\n"
+;;                                                  (cdr item))
+;;                 (cdr item)))
+;;              new-alist)))
+;;     new-alist))
 
 (defun sysinfo-environs-get-all-env ()
   "Unused function for $ENV things."
   (cons "*environment variables*"
-        ;; (sysinfo-environs-split-paths-with-newlines
-         (sysinfo-environs-list-of-string-equals-string-into-alist
-          (sysinfo-environs-newlines-string-into-line-list
-           (shell-command-to-string "printenv")))))
+        (cl-remove '("") 
+                   (sysinfo-environs-list-of-string-equals-string-into-alist
+                    (sysinfo-environs-newlines-string-into-line-list
+                     (shell-command-to-string "printenv")))
+                   :test 'equal)))
 
-;; (sysinfo-environs-sysinfo (list (sysinfo-environs-get-all-env)q))
+
+
+;; wm/de:
+;; check $XDG_CURRENT_DESKTOP (need to check if contains ":
+;; if """, check $XDG_SESSION_DESKTOP
+;; if "", check $DESKTOP_SESSION
+;; if "", check if `wmctrl' exists, if so, `wmctrl -m' outputs, e.g.:
+;; Name: EXWM
+;; Class: N/A
+;; PID: N/A
+;; Window manager's "showing the desktop" mode: N/A
+
+;; (sysinfo-environs-sysinfo (list (sysinfo-environs-get-all-env)))
+;; (setq sysinf-environs-env (list (sysinfo-environs-get-all-env)))
 
 ;;; other old bits
 ;; (defun sysinfo-environs-os-release-look-up-field (&optional field)
