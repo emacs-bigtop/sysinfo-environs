fe4d40b78e5a66329cace295ba1a55bf043b4bb8
Benjamin Slade <slade@lambda-y.net>
2025-03-27 13:00:40 -0500

    fix header line

--- sysinfo-environs.el
+++ sysinfo-environs.el
@@ -1,4 +1,4 @@
-;; sysinfo-environs.el --- Display system information in various formats  -*- lexical-binding: t; -*-
+;;; sysinfo-environs.el --- Display system information in various formats  -*- lexical-binding: t; -*-
 
 ;; System Information and Environs
 
@@ -66,18 +66,6 @@
   "If set, don't single-window neofetch-info."
   :group 'sysinfo-environs
   :type 'boolean)
-
-(defcustom sysinfo-environs-dataset-alist
-  "An alist of all of the datasources and some names for them."
-  '(("emacs-info" . (sysinfo-environs-emacs-known-sysinfo))
-    ("emacs-self-info" . (sysinfo-environs-emacs-self-info))
-    ("uname-info" . (sysinfo-environs-parse-uname-info))
-    ("os-release-info" . (sysinfo-environs-parse-os-release))
-    ("env-vars-info" . (sysinfo-environs-get-all-env))
-    ("sys-devices-virtual-dmi-info" . (sysinfo-environs-read-sys-devices-virtual-dmi-ids)))
-  :group 'sysinfo-environs
-  :type 'list)
-
 
 ;;;; Helper functions
 ;;;###autoload
@@ -509,6 +497,16 @@ Assumes operating system ID to be `OS-ID-NAME'.
 ;;           (cons
 ;;            (cons )
 ;;            interfaces))))
+
+;;;;; list of all sources
+(defvar sysinfo-environs-dataset-alist
+  '(("emacs-info" . (sysinfo-environs-emacs-known-sysinfo))
+    ("emacs-self-info" . (sysinfo-environs-emacs-self-info))
+    ("uname-info" . (sysinfo-environs-parse-uname-info))
+    ("os-release-info" . (sysinfo-environs-parse-os-release))
+    ("env-vars-info" . (sysinfo-environs-get-all-env))
+    ("sys-devices-virtual-dmi-info" . (sysinfo-environs-read-sys-devices-virtual-dmi-ids)))
+  "An alist of all of the datasources and some names for them.")
 
 (defun sysinfo-environs-dataset-bare-list ()
   "Strips out the pretty names from `sysinfo-environs-dataset-alist'.
